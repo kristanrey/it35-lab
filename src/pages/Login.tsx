a23a7a7bfd0b4d740552920c0fd63a2254670486
kristanrey <kristanreyflores08@gmail.com>
2025-02-13 01:07:24 +0800

    Added update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,28 +1,35 @@
 import { 
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
-  
-  const Login: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Login</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default Login;
+  IonButton,
+  IonButtons,
+    IonContent, 
+    IonHeader, 
+    IonMenuButton, 
+    IonPage, 
+    IonTitle, 
+    IonToolbar, 
+    useIonRouter
+} from '@ionic/react';
+
+const Login: React.FC = () => {
+  const navigation = useIonRouter();
+
+  const doLogin = () => {
+      navigation.push('/it35-lab/app','forward','replace');
+  }
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Login</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className='ion-padding'>
+          <IonButton onClick={() => doLogin()} expand="full">
+              Login
+          </IonButton>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Login;
