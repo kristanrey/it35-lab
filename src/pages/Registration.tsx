de463eb1a45d1dd7c6af7722bc856d47f62c9c4a
kristanrey <kristanreyflores08@gmail.com>
2025-03-10 17:49:00 +0800

    Added update in feeds.tsx

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -76,7 +76,7 @@ const Register: React.FC = () => {
   <IonRow>
     <IonCol className="ion-text-center">
       <IonAvatar style={{ width: "100px", height: "100px", margin: "auto" }}>
-        <img src="https://cdn-icons-png.flaticon.com/128/16951/16951316.png" alt="Avatar" style={{ width: "100%", height: "100%", objectFit: "cover" }} />
+        <img src="https://scontent.fceb8-1.fna.fbcdn.net/v/t39.30808-1/373517144_10006395102767259_3385756574041204882_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=100&ccb=1-7&_nc_sid=e99d92&_nc_eui2=AeHBrFaQij8eDgSuEsVdtzwujoTTnHYPDyiOhNOcdg8PKDGM2CFS1CwOynjLEQU__nSeMWHgMNodej7rK_hmYIj3&_nc_ohc=OtcpRFFXc10Q7kNvgF-U2AF&_nc_oc=Adj7hKROWsqIU5z2Gj_n1XlsnoMrW5sgS0ex3TNXvLdF2ASFxGK8U6py5AIlwRcNNn0_VgNwnSHfqOz1HIAnEumU&_nc_zt=24&_nc_ht=scontent.fceb8-1.fna&_nc_gid=AscMIHAs1IBSSXGO0o1OjgF&oh=00_AYHRNwOAHBZnpgJDS0h8u2xd9DS6Eg4cWrnJCK6buA5BDQ&oe=67D495D4" alt="Avatar" style={{ width: "100%", height: "100%", objectFit: "cover" }} />
       </IonAvatar>
     </IonCol>
   </IonRow>
