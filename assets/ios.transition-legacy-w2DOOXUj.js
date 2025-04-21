8cf197e3457eb648161005f46fc76309a0038cd0
kristanrey <kristanreyflores08@gmail.com>
2025-04-21 12:00:40 +0800

    Updates

--- assets/ios.transition-legacy-w2DOOXUj.js
+++ assets/ios.transition-legacy-w2DOOXUj.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-qm3cCZAZ.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-rmRSUzpw.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
