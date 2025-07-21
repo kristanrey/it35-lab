111e198b905046006e32ca1ac3d1e99c4160c451
kristanrey <kristanreyflores08@gmail.com>
2025-07-21 20:09:52 +0800

    added package

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+// Replace this with your actual Google OAuth Client ID from https://console.cloud.google.com/apis/credentials
+const GOOGLE_CLIENT_ID = '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';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -17,6 +17,9 @@ import {
   useIonToast,
 } from "@ionic/react";
 import { useState } from "react";
+import { GoogleLogin } from '@react-oauth/google';
+import jwtDecode from 'jwt-decode';
+import FacebookLogin from '@greatsumini/react-facebook-login';
 
 const Login: React.FC = () => {
   const router = useIonRouter();
@@ -58,15 +61,19 @@ const Login: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-      <IonGrid style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-  <IonRow>
-    <IonCol className="ion-text-center">
-      <IonAvatar style={{ width: "100px", height: "100px", margin: "auto" }}>
-        <img src="https://scontent.fceb8-1.fna.fbcdn.net/v/t39.30808-1/373517144_10006395102767259_3385756574041204882_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=100&ccb=1-7&_nc_sid=e99d92&_nc_eui2=AeHBrFaQij8eDgSuEsVdtzwujoTTnHYPDyiOhNOcdg8PKDGM2CFS1CwOynjLEQU__nSeMWHgMNodej7rK_hmYIj3&_nc_ohc=OtcpRFFXc10Q7kNvgF-U2AF&_nc_oc=Adj7hKROWsqIU5z2Gj_n1XlsnoMrW5sgS0ex3TNXvLdF2ASFxGK8U6py5AIlwRcNNn0_VgNwnSHfqOz1HIAnEumU&_nc_zt=24&_nc_ht=scontent.fceb8-1.fna&_nc_gid=AscMIHAs1IBSSXGO0o1OjgF&oh=00_AYHRNwOAHBZnpgJDS0h8u2xd9DS6Eg4cWrnJCK6buA5BDQ&oe=67D495D4" alt="Avatar" style={{ width: "100%", height: "100%", objectFit: "cover" }} />
-      </IonAvatar>
-    </IonCol>
-  </IonRow>
-</IonGrid>
+        <IonGrid style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+          <IonRow>
+            <IonCol className="ion-text-center">
+              <IonAvatar style={{ width: "100px", height: "100px", margin: "auto" }}>
+                <img
+                  src="https://scontent.fceb8-1.fna.fbcdn.net/v/t39.30808-1/373517144_10006395102767259_3385756574041204882_n.jpg"
+                  alt="Avatar"
+                  style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                />
+              </IonAvatar>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
 
         <IonInput label="Email" type="email" value={email} onIonChange={(e) => setEmail(e.detail.value!)} placeholder="Enter email" />
         <IonInput type="password" label="Password" value={password} onIonChange={(e) => setPassword(e.detail.value!)} placeholder="Enter password">
@@ -76,6 +83,85 @@ const Login: React.FC = () => {
           Login
         </IonButton>
 
+        <IonText color="medium" style={{ display: "block", marginTop: "10px", textAlign: "center" }}>
+          or
+        </IonText>
+
+        {/* Google Login Button */}
+        <div style={{ display: "flex", justifyContent: "center", marginTop: "10px" }}>
+          <GoogleLogin
+            onSuccess={(credentialResponse) => {
+              if (credentialResponse.credential) {
+                const decoded: any = jwtDecode(credentialResponse.credential);
+                const googleEmail = decoded.email;
+                const users = JSON.parse(localStorage.getItem("users") || "[]");
+                const existingUser = users.find((u: any) => u.email === googleEmail);
+
+                if (!existingUser) {
+                  users.push({ email: googleEmail, password: "google_oauth" });
+                  localStorage.setItem("users", JSON.stringify(users));
+                }
+
+                presentToast({
+                  message: `Welcome ${decoded.name}!`,
+                  duration: 2000,
+                  position: "top",
+                  color: "success",
+                });
+
+                router.push("/it35-lab/app");
+              }
+            }}
+            onError={() => {
+              setAlertMessage("Google Sign-In failed. Please try again.");
+              setShowAlert(true);
+            }}
+          />
+        </div>
+
+        {/* Facebook Login Button */}
+        <div style={{ marginTop: "15px", textAlign: "center" }}>
+          <FacebookLogin
+            appId="YOUR_FACEBOOK_APP_ID"
+            onSuccess={(response) => {
+              const fbEmail = response.email;
+              const users = JSON.parse(localStorage.getItem("users") || "[]");
+              const existingUser = users.find((u: any) => u.email === fbEmail);
+
+              if (!existingUser) {
+                users.push({ email: fbEmail, password: "facebook_oauth" });
+                localStorage.setItem("users", JSON.stringify(users));
+              }
+
+              presentToast({
+                message: `Welcome ${response.name || 'Facebook User'}!`,
+                duration: 2000,
+                position: "top",
+                color: "success",
+              });
+
+              router.push("/it35-lab/app");
+            }}
+            onFail={(err) => {
+              setAlertMessage("Facebook login failed.");
+              setShowAlert(true);
+            }}
+            style={{
+              width: '250px',
+              padding: '10px',
+              backgroundColor: '#3b5998',
+              color: 'white',
+              border: 'none',
+              borderRadius: '5px',
+              fontWeight: 'bold',
+              cursor: 'pointer',
+              display: 'inline-block'
+            }}
+          >
+            Login with Facebook
+          </FacebookLogin>
+        </div>
+
         <IonText color="primary" style={{ display: "block", marginTop: "15px", textAlign: "center" }}>
           Don't have an account?{" "}
           <span onClick={goToRegister} style={{ textDecoration: "underline", cursor: "pointer" }}>
@@ -83,7 +169,6 @@ const Login: React.FC = () => {
           </span>
         </IonText>
 
-        {/* Alert for incorrect login */}
         <IonAlert isOpen={showAlert} message={alertMessage} buttons={["OK"]} onDidDismiss={() => setShowAlert(false)} />
       </IonContent>
     </IonPage>
