90b0fc68f313114c28642533ec33d9984bd67e81
kristanrey <kristanreyflores08@gmail.com>
2025-07-22 21:12:38 +0800

    added update layout

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -18,8 +18,7 @@ import {
   useIonToast,
 } from "@ionic/react";
 import { useState } from "react";
-import { GoogleLogin } from '@react-oauth/google';
-import { jwtDecode } from 'jwt-decode'; // ✅ use jwtDecode from v4.x
+import { useGoogleLogin } from '@react-oauth/google';
 import FacebookLogin from '@greatsumini/react-facebook-login';
 
 const Login: React.FC = () => {
@@ -54,6 +53,42 @@ const Login: React.FC = () => {
     router.push("/it35-lab/register");
   };
 
+  const loginWithGoogle = useGoogleLogin({
+    onSuccess: (tokenResponse) => {
+      if (tokenResponse.access_token) {
+        fetch('https://www.googleapis.com/oauth2/v3/userinfo', {
+          headers: {
+            Authorization: `Bearer ${tokenResponse.access_token}`,
+          },
+        })
+          .then(res => res.json())
+          .then(decoded => {
+            const googleEmail = decoded.email;
+            const users = JSON.parse(localStorage.getItem("users") || "[]");
+            const existingUser = users.find((u: any) => u.email === googleEmail);
+
+            if (!existingUser) {
+              users.push({ email: googleEmail, password: "google_oauth" });
+              localStorage.setItem("users", JSON.stringify(users));
+            }
+
+            presentToast({
+              message: `Welcome ${decoded.name}!`,
+              duration: 2000,
+              position: "top",
+              color: "success",
+            });
+
+            router.push("/it35-lab/app");
+          });
+      }
+    },
+    onError: () => {
+      setAlertMessage("Google Sign-In failed. Please try again.");
+      setShowAlert(true);
+    },
+  });
+
   return (
     <IonPage>
       <IonHeader>
@@ -68,7 +103,7 @@ const Login: React.FC = () => {
             <IonCol className="ion-text-center">
               <IonAvatar style={{ width: "100px", height: "100px", margin: "auto" }}>
                 <img
-                  src="https://scontent.fceb8-1.fna.fbcdn.net/v/t39.30808-1/373517144_10006395102767259_3385756574041204882_n.jpg"
+                  src="https://scontent.fceb8-1.fna.fbcdn.net/v/t1.15752-9/520858659_2057475981322730_7566595731853589810_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=101&ccb=1-7&_nc_sid=0024fc&_nc_eui2=AeFuksu5rIMWeIBOpQSZIjR2HYSkklb8iyAdhKSSVvyLIE5OxnXB3wNnDcIRaLnKZBzp_JNyGICV4mxudvTB-Ll0&_nc_ohc=gB5lm7hDNn0Q7kNvwHbL1qn&_nc_oc=AdkRQF7KphJps_oWOc-2dc7Bl0-sgf-JUgNkFsXbHhkQ3X_qqBRstZMQsboBI58zo7k&_nc_ad=z-m&_nc_cid=5917&_nc_zt=23&_nc_ht=scontent.fceb8-1.fna&oh=03_Q7cD2wFxQdiQOeWgBqILAxfFJZ0mjqYFysHskhRfLdJ72EUE6g&oe=68A6ED2C"
                   alt="Avatar"
                   style={{ width: "100%", height: "100%", objectFit: "cover" }}
                 />
@@ -102,40 +137,31 @@ const Login: React.FC = () => {
           or
         </IonText>
 
-        {/* Google Login */}
-        <div style={{ display: "flex", justifyContent: "center", marginTop: "10px" }}>
-          <GoogleLogin
-            onSuccess={(credentialResponse) => {
-              if (credentialResponse.credential) {
-                const decoded: any = jwtDecode(credentialResponse.credential);
-                const googleEmail = decoded.email;
-                const users = JSON.parse(localStorage.getItem("users") || "[]");
-                const existingUser = users.find((u: any) => u.email === googleEmail);
-
-                if (!existingUser) {
-                  users.push({ email: googleEmail, password: "google_oauth" });
-                  localStorage.setItem("users", JSON.stringify(users));
-                }
-
-                presentToast({
-                  message: `Welcome ${decoded.name}!`,
-                  duration: 2000,
-                  position: "top",
-                  color: "success",
-                });
-
-                router.push("/it35-lab/app");
-              }
+        {/* Google + Facebook Logos Side by Side */}
+        <div style={{ display: "flex", justifyContent: "center", gap: "20px", marginTop: "15px" }}>
+          {/* Google Logo */}
+          <div
+            onClick={() => loginWithGoogle()}
+            style={{
+              cursor: "pointer",
+              width: "50px",
+              height: "50px",
+              borderRadius: "50%",
+              overflow: "hidden",
             }}
-            onError={() => {
-              setAlertMessage("Google Sign-In failed. Please try again.");
-              setShowAlert(true);
-            }}
-          />
-        </div>
+          >
+            <img
+              src="https://developers.google.com/identity/images/g-logo.png"
+              alt="Google Login"
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+              }}
+            />
+          </div>
 
-        {/* Facebook Login */}
-        <div style={{ marginTop: "15px", textAlign: "center" }}>
+          {/* Facebook Logo */}
           <FacebookLogin
             appId="741074022014246"
             autoLoad={false}
@@ -169,18 +195,22 @@ const Login: React.FC = () => {
             scope="public_profile,email"
             fields="name,email"
             style={{
-              width: '250px',
-              padding: '10px',
-              backgroundColor: '#3b5998',
-              color: 'white',
-              border: 'none',
-              borderRadius: '5px',
-              fontWeight: 'bold',
-              cursor: 'pointer',
-              display: 'inline-block',
+              background: "none",
+              border: "none",
+              padding: 0,
+              cursor: "pointer",
             }}
           >
-            Login with Facebook
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/0/05/Facebook_Logo_%282019%29.png"
+              alt="Facebook Login"
+              style={{
+                width: "50px",
+                height: "50px",
+                borderRadius: "50%",
+                objectFit: "cover",
+              }}
+            />
           </FacebookLogin>
         </div>
 
@@ -191,7 +221,12 @@ const Login: React.FC = () => {
           </span>
         </IonText>
 
-        <IonAlert isOpen={showAlert} message={alertMessage} buttons={["OK"]} onDidDismiss={() => setShowAlert(false)} />
+        <IonAlert
+          isOpen={showAlert}
+          message={alertMessage}
+          buttons={["OK"]}
+          onDidDismiss={() => setShowAlert(false)}
+        />
       </IonContent>
     </IonPage>
   );
