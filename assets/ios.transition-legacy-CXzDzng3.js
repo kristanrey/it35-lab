df01d2d699bad9bcda6ea76e20ec4ca501f167ac
kristanrey <kristanreyflores08@gmail.com>
2025-07-22 22:32:29 +0800

    Updates

--- assets/ios.transition-legacy-CXzDzng3.js
+++ assets/ios.transition-legacy-CXzDzng3.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-NBXKabSZ.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BWYcZXGc.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-BvfKo2tz.js
+++ assets/swipe-back-legacy-BvfKo2tz.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-NBXKabSZ.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-BWYcZXGc.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
